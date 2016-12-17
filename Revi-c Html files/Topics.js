4e7ab57aaae7666b7889439e4a9b08ff8a011e1c
Theafrokidd <theagroafro@gmail.com>
2016-12-17 16:34:28 +0000

    Front end 17/12/16 commit
    Html files for the front end of the app

---
+++ Revi-c Html files/Topics.js
@@ -0,0 +1,91 @@
+
+myFunction([ 
+								{"subject":"Maths":
+												{"topics": "Algerbra":
+																{"subptopic":"Simulteanous Equations"},
+																{"subtopic": "Linear Equations"},
+																{"subtopic": "Quadratic Equations"},
+																{"subtopic":"Simplifying Algebra"},
+																{"subtopic":"Arithmetic and Geometric progressions"}
+												},
+												{"topics": "Ratio, proportion and rates of change":
+																	{"subtopic":"Fractions of an Amount"},
+																	{"subtopic":"Writing and Simplifying Ratio"},
+																	{"subtopic":"Ratio"},
+																	{"subtopic":"Writing a Ratio as a Fraction or Linear Function"},
+																	{"subtopic":"Proportion"}
+												},
+												{"topics": "Geometry and measures":
+																	{"subtopic":"Angles"},
+																	{"subtopic":"Area"},
+																	{"subtopic":"Perimeter"}
+												},
+												{"topics": "Probability":
+																	{"subtopic":""},
+																	{"subtopic":""},
+																	{"subtopic":""},
+																	{"subtopic":""},
+																	{"subtopic":""}
+												},
+												{"topics": "Statistics":
+																		{"subtopic":""},
+																		{"subtopic":""},
+																		{"subtopic":""},
+																		{"subtopic":""},
+																		{"subtopic":""}
+												},
+												{"topics": "Number":
+																		{"subtopic":""},
+																		{"subtopic":""},
+																		{"subtopic":""},
+																		
+												}
+								},
+								
+								
+								{"Subject":"English":
+													{"topics": "":
+																		{"subtopic":""},
+																		{"subtopic":""},
+																		{"subtopic":""}
+													{"topics": "":
+																		{"subtopic":""},
+																		{"subtopic":""},
+																		{"subtopic":""}
+													{"topics": "":
+																		{"subtopic":""},
+																		{"subtopic":""},
+																		{"subtopic":""}
+													}
+								},
+
+
+
+
+
+
+
+							},
+								{"Subject":"Science":
+													{"topics": "":
+																		{"subtopic":""},
+																		{"subtopic":""},
+																		{"subtopic":""}
+													{"topics": "":
+																		{"subtopic":""},
+																		{"subtopic":""},
+																		{"subtopic":""}
+													{"topics": "":
+																		{"subtopic":""},
+																		{"subtopic":""},
+																		{"subtopic":""}
+													}
+								},
+
+
+
+
+
+
+
+							},
